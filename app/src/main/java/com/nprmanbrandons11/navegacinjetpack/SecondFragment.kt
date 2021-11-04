2baf5e970c84c5a27271ada025a61613b4702429
barcenas.proteco@gmail.com <Edgar1515>
2021-11-03 20:08:25 -0600

    Cambios

--- app/src/main/java/com/nprmanbrandons11/navegacinjetpack/SecondFragment.kt
+++ app/src/main/java/com/nprmanbrandons11/navegacinjetpack/SecondFragment.kt
@@ -12,32 +12,14 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.nprmanbrandons11.navegacinjetpack.databinding.FragmentSecondBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SecondFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SecondFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    var argumento:String? = null
-    var argumentoscore:String? = null
+
+    private var argumento:String? = null
+    private var argumentoscore:String? = null
     private var _binding : FragmentSecondBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,43 +39,25 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         if(argumentoscore != null){
-
-        binding.scoretv.text = argumentoscore}
+            binding.scoretv.text = argumentoscore}
         else{
             binding.scoretv.text = "0"
         }
 
         binding.usuarioNombre.text = argumento
+
         binding.botonCerrarsesion.setOnClickListener{
             findNavController().navigate(R.id.action_secondFragment_to_fragmentMain)
-
         }
+
         binding.botonCambiar.setOnClickListener{
             findNavController().navigate(R.id.action_secondFragment_to_loginFragment)
 
         }
+
         binding.botonIrbolado.setOnClickListener{
             findNavController().navigate(R.id.action_secondFragment_to_bolado)
-
         }
     }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
